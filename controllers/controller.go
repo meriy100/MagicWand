522529bf43d176cd5ccf2129b2617e4ebfa9ab1d
meriy100 <ttattataa@gmail.com>
2024-01-14 22:53:15 +0900

    fix: tui を github.com/charmbracelet/bubbles に変更

--- controllers/controller.go
+++ controllers/controller.go
@@ -1,12 +1,17 @@
 package controllers
 
 import (
+	"fmt"
+	"github.com/charmbracelet/bubbles/cursor"
+	"github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 	"github.com/cockroachdb/errors"
-	"github.com/manifoldco/promptui"
 	"github.com/meriy100/magicwand/entities"
 	"github.com/meriy100/magicwand/goinital"
 	"github.com/meriy100/magicwand/terraforms"
 	"os"
+	"strings"
 )
 
 type Controller struct {
@@ -16,112 +21,194 @@ func NewController() *Controller {
 	return &Controller{}
 }
 
-func (c *Controller) Run() error {
+var (
+	focusedStyle        = lipgloss.NewStyle().Foreground(lipgloss.Color("205"))
+	blurredStyle        = lipgloss.NewStyle().Foreground(lipgloss.Color("240"))
+	cursorStyle         = focusedStyle.Copy()
+	noStyle             = lipgloss.NewStyle()
+	helpStyle           = blurredStyle.Copy()
+	cursorModeHelpStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("244"))
+
+	focusedButton = focusedStyle.Copy().Render("[ Submit ]")
+	blurredButton = fmt.Sprintf("[ %s ]", blurredStyle.Render("Submit"))
+)
+
+type model struct {
+	focusIndex int
+	inputs     []textinput.Model
+	cursorMode cursor.Mode
+}
+
+func initialModel() model {
+	m := model{
+		inputs: make([]textinput.Model, 3),
+	}
+
+	var t textinput.Model
+	for i := range m.inputs {
+		t = textinput.New()
+		t.Cursor.Style = cursorStyle
+		t.CharLimit = 32
+
+		switch i {
+		case 0:
+			t.Placeholder = "PackageName"
+			t.Focus()
+			t.PromptStyle = focusedStyle
+			t.TextStyle = focusedStyle
+		case 1:
+			t.Placeholder = "AppName"
+			t.CharLimit = 64
+		case 2:
+			t.Placeholder = "GCP ProjectID"
+			t.CharLimit = 64
+		}
+
+		m.inputs[i] = t
+	}
+
+	return m
+}
+func (m model) Init() tea.Cmd {
+	return textinput.Blink
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "esc":
+			return m, tea.Quit
+
+		// Change cursor mode
+		case "ctrl+r":
+			m.cursorMode++
+			if m.cursorMode > cursor.CursorHide {
+				m.cursorMode = cursor.CursorBlink
+			}
+			cmds := make([]tea.Cmd, len(m.inputs))
+			for i := range m.inputs {
+				cmds[i] = m.inputs[i].Cursor.SetMode(m.cursorMode)
+			}
+			return m, tea.Batch(cmds...)
+
+		// Set focus to next input
+		case "tab", "shift+tab", "enter", "up", "down":
+			s := msg.String()
+
+			// Did the user press enter while the submit button was focused?
+			// If so, exit.
+			if s == "enter" && m.focusIndex == len(m.inputs) {
+				return m, func() tea.Msg { return runCreateFiles(m) }
+			}
+
+			// Cycle indexes
+			if s == "up" || s == "shift+tab" {
+				m.focusIndex--
+			} else {
+				m.focusIndex++
+			}
+
+			if m.focusIndex > len(m.inputs) {
+				m.focusIndex = 0
+			} else if m.focusIndex < 0 {
+				m.focusIndex = len(m.inputs)
+			}
+
+			cmds := make([]tea.Cmd, len(m.inputs))
+			for i := 0; i <= len(m.inputs)-1; i++ {
+				if i == m.focusIndex {
+					// Set focused state
+					cmds[i] = m.inputs[i].Focus()
+					m.inputs[i].PromptStyle = focusedStyle
+					m.inputs[i].TextStyle = focusedStyle
+					continue
+				}
+				// Remove focused state
+				m.inputs[i].Blur()
+				m.inputs[i].PromptStyle = noStyle
+				m.inputs[i].TextStyle = noStyle
+			}
+
+			return m, tea.Batch(cmds...)
+		}
+	}
+
+	// Handle character input and blinking
+	cmd := m.updateInputs(msg)
+
+	return m, cmd
+}
+
+func (m *model) updateInputs(msg tea.Msg) tea.Cmd {
+	cmds := make([]tea.Cmd, len(m.inputs))
+
+	// Only text inputs with Focus() set will respond, so it's safe to simply
+	// update all of them here without any further logic.
+	for i := range m.inputs {
+		m.inputs[i], cmds[i] = m.inputs[i].Update(msg)
+	}
+
+	return tea.Batch(cmds...)
+}
+
+type errMsg error
+
+func runCreateFiles(m model) tea.Msg {
 	interactor := goinital.NewInteractor()
-	cs := entities.ConfigSet{}
-	var err error
-
-	validate := func(input string) error {
-		if len(input) < 1 {
-			return errors.New("Invalid. package name is required")
-		}
-		return nil
+	cs := entities.ConfigSet{
+		PackageName: m.inputs[0].Value(),
+		AppName:     m.inputs[1].Value(),
+		GCPConfig: entities.GCPConfig{
+			ProjectID: m.inputs[2].Value(),
+		},
 	}
-
-	prompt := promptui.Prompt{
-		Label:    "package name",
-		Validate: validate,
-	}
-
-	cs.PackageName, err = prompt.Run()
-
-	if err != nil {
-		return errors.Wrapf(err, "Prompt failed %v")
-	}
-
 	if err := interactor.InitGomod(cs.PackageName); err != nil {
-		return err
-	}
-
-	validate = func(input string) error {
-		if len(input) < 1 {
-			return errors.New("Invalid. package name is required")
-		}
-		return nil
-	}
-
-	prompt = promptui.Prompt{
-		Label:    "application command name",
-		Validate: validate,
-	}
-
-	cs.AppName, err = prompt.Run()
-
-	if err != nil {
-		return errors.Wrapf(err, "Prompt failed %v")
-	}
-
-	options := []struct {
-		Name  string
-		Value entities.AppType
-	}{
-		{
-			Name:  "Rest",
-			Value: entities.Rest,
-		},
-		{
-			Name:  "GraphQL",
-			Value: entities.GraphQL,
-		},
-	}
-	templates := &promptui.SelectTemplates{
-		Active:   "{{ .Name | green }} ",
-		Inactive: "{{ .Name }}",
-	}
-
-	promptS := promptui.Select{
-		Label:        "application type",
-		Items:        options,
-		Templates:    templates,
-		HideSelected: true,
-	}
-
-	idx, _, err := promptS.Run()
-	cs.AppType = options[idx].Value
-
-	if err != nil {
-		return errors.Wrapf(err, "Prompt failed %v")
+		return errMsg(err)
 	}
 
 	if err := interactor.CreateMain(cs.AppName, cs.AppType); err != nil {
-		return err
-	}
-
-	validate = func(input string) error {
-		if len(input) < 1 {
-			return errors.New("Invalid. GCP project id is required")
-		}
-		return nil
-	}
-
-	prompt = promptui.Prompt{
-		Label:    "GCP Project ID",
-		Validate: validate,
-	}
-
-	cs.GCPConfig.ProjectID, err = prompt.Run()
-
-	if err != nil {
-		return errors.Wrapf(err, "Prompt failed %v")
+		return errMsg(err)
 	}
 
 	if err := os.Mkdir("terraform", 0755); err != nil {
-		return errors.Wrapf(err, "make directory failed")
+		return errMsg(errors.Wrapf(err, "make directory failed"))
 	}
 
 	backend := terraforms.NewBackend()
 	if err := backend.Create(cs.GCPConfig); err != nil {
-		return err
+		return errMsg(err)
+	}
+	return tea.Quit()
+}
+
+func (m model) View() string {
+	var b strings.Builder
+
+	for i := range m.inputs {
+		b.WriteString(m.inputs[i].View())
+		if i < len(m.inputs)-1 {
+			b.WriteRune('\n')
+		}
+	}
+
+	button := &blurredButton
+	if m.focusIndex == len(m.inputs) {
+		button = &focusedButton
+	}
+	fmt.Fprintf(&b, "\n\n%s\n\n", *button)
+
+	b.WriteString(helpStyle.Render("cursor mode is "))
+	b.WriteString(cursorModeHelpStyle.Render(m.cursorMode.String()))
+	b.WriteString(helpStyle.Render(" (ctrl+r to change style)"))
+
+	return b.String()
+}
+
+func (c *Controller) Run() error {
+	if _, err := tea.NewProgram(initialModel()).Run(); err != nil {
+		fmt.Printf("could not start program: %s\n", err)
+		os.Exit(1)
 	}
 
 	return nil
